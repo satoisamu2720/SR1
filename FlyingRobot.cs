cfed401c4df008bfba759997edab1c676468bb06
and lloyd <andlloyd.hotaru.2720@gmail.com>
2024-11-15 11:57:13 +0900

    ok

---
+++ FlyingRobot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1
+{
+    internal class FlyingRobot : Robot
+    {
+      public  FlyingRobot(string name) 
+        {
+            this.name = name;
+        }
+
+        public void DroBomb()
+        {
+            if (powerStatus)
+            {
+                Console.WriteLine("{0}は、爆弾を落とした!", name);
+            }
+            else
+            {
+                Console.WriteLine("{0}は、電源offなので爆弾投下はつかえません..", name);
+            }
+        }
+    }
+}
---
+++ Robot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1
+{
+    internal class Robot
+    {
+        protected string name = "";
+        protected bool powerStatus = false;
+
+        public void PowerOn()
+        {
+            powerStatus = true;
+            Console.WriteLine("{0}は、起動した‼", name);
+
+        }
+
+        public void PowerOff() { 
+            powerStatus = false;
+            Console.WriteLine("{0}は、停止した.." ,name);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public bool GetPowerStatus() 
+        {
+            return powerStatus;
+        }
+    }
+}
