1311f9c8d5ef89a2a6558adad0af6e9d75a9df46
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2024-12-11 15:54:25 +0900

    ok

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1
+{
+    internal class Player : Item
+    {
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
