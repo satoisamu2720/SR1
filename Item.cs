323946369ac2cc8fd9878026ab4edb02ea9e30db
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2024-12-11 15:30:07 +0900

    ok

---
+++ Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SR1
+{
+    internal class Item
+    {
+        protected string name;
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void Use()
+        {
+            Console.WriteLine("{0}を使用した", name);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace SR1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello C# World!!");
+           Item item = new Item("つるはし");
+
+            item.Use();
+
             Console.ReadLine();
         }
     }
